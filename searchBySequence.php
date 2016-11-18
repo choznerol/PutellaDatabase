2e8118a8f4e593156197ab4a525aad9941bd1c1e
lc1024 <b02611002@ntu.edu.tw>
2016-11-18 22:25:04 +0800

    exp

--- searchBySequence.php
+++ searchBySequence.php
@@ -107,8 +107,15 @@ tr:nth-child(even) {
             system("pwd");
 
             exec("echo \"$sequence\" > $path_prefix$job_id.fasta");
-            exec("blastn -db /home/callsobing/putella/putella_cufflinks -query $path_prefix$job_id.fasta -outfmt 6 -num_threads 4 -evalue 0.00000001 -perc_identity 100 > $path_prefix$job_id.output 2> $path_prefix$job_id.err");
-            exec("blastn -db /home/callsobing/putella/putella_cufflinks -query var/example.fasta -outfmt 6 -num_threads 4 -evalue 0.00000001 -perc_identity 100 > var/example$job_id.output 2> var/example$path_prefix$job_id.err");
+
+            exec("blastn -db /home/callsobing/putella/putella_cufflinks -query $path_prefix$job_id.fasta -outfmt 6 -num_threads 4 -evalue 0.00000001 -perc_identity 100 
+            > $path_prefix$job_id.output 
+            2> $path_prefix$job_id.err");
+
+            exec("blastn -db /home/callsobing/putella/putella_cufflinks -query var/example.fasta -outfmt 6 -num_threads 4 -evalue 0.00000001 -perc_identity 100 
+            > var/example_$job_id.output 
+            2> var/example_$path_prefix$job_id.err");
+
             exec("cat $path_prefix$job_id.output", $output_file);
 
             exec("blastn -db /home/callsobing/putella/putella_cufflinks -query var/example.fasta -outfmt 6 -num_threads 4 -evalue 0.00000001 -perc_identity 100", $example_output);
