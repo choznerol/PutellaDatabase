8055287ee6a767cdc7e0dedb83bc6119f9c530c1
lc1024 <b02611002@ntu.edu.tw>
2016-11-18 18:42:36 +0800

    ls

--- searchBySequence.php
+++ searchBySequence.php
@@ -122,7 +122,12 @@ tr:nth-child(even) {
 
             exec("pwd 2>&1', $output");
             print_r($output);
-            echo "pwd: $output";
+            echo "pwd: $output\n";
+
+            exec("ls 2>&1', $output");
+            print_r($output);
+            echo "ls: $output\n";
+
             exec("mkdir -m 777 $path_prefix$job_id");
             exec("echo \"$sequence\" > $path_prefix$job_id.fasta");
             exec("blastn -db ~/../callsobing/putella/putella_cufflinks -query $path_prefix$job_id.fasta -outfmt 6 -num_threads 4 -evalue 0.00000001 -perc_identity 100 >$path_prefix$job_id.output");
